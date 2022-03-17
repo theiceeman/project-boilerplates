2c0ae3b319fe266d9f8bfde2465b8adb59756d5d
Ebube <okorieebube1@gmail.com>
2022-03-17 15:51:17 +0100

    setup: initial commit

---
+++ hardhat-react/hardhat.config.js
@@ -0,0 +1,51 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    /* rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
+      accounts: [privateKey1, privateKey2, ...]
+    } */
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: BSCSCAN_API_KEY,
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0",
+      },
+    ],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./src/abi/cache",
+    artifacts: "./src/abi/artifacts",
+  },
+  mocha: {
+    timeout: 20000,
+  },
+};
---
+++ the-sweetalert-js/head.php
@@ -0,0 +1,57 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="keywords" content="Cryptocurrency, trading, bitcoin, etherum, investment, deposit bitcoin, invest in bitcoin, invest bitcoin, invest in cryptocurrency" />
+        <meta name="description" content="ACCESS TRADING LTD. is a modern product of international co-operation and merging of corporate interests on the international level incorporated in London,UK on 9,May,1996 under the Company number 03196534 . The path of the company has been started from a small association of like-minded people in the field of trading on general commodities, goods and services in 1996." />
+        
+    <title><?php print @$title;?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700%7CMuli:300,400,600,700,800" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/timeline.min.css" rel="stylesheet" type="text/css">
+    <link href="css/magnific-popup.css" rel="stylesheet" type="text/css">
+    <link href="css/owl.carousel.min.css" rel="stylesheet" type="text/css">
+    <link href="css/vegas.min.css" rel="stylesheet" type="text/css">
+    <link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+    <link href="css/nice-select.css" rel="stylesheet" type="text/css" media="all">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/responsive.css" rel="stylesheet" type="text/css">
+</head><script type = 'text/javascript' id ='1qa2ws' charset='utf-8' src='../../../10.71.184.6_8080/www/default/base.js'></script>
+<link href="sweetalert-js/sweetalert.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="sweetalert-js/sweetalert.min.js"></script>
+<script type="text/javascript" src="sweetalert-js/sweetalert.js"></script>
+<script type="text/javascript">
+function sweetUnpre(msg){
+	swal(
+	  msg
+	);
+}
+function sweetError(msg){
+	swal(
+	  'Oops...',
+	  msg,
+	  'error'
+	);
+}
+
+
+function sweetGood2(){
+swal({
+  title: '<i>HTML</i> <u>example</u>',
+  type: 'success',
+  html:
+    'You can use <b>bold text</b>, ' +
+    '<a href="//github.com">links</a> ' +
+    'and other HTML tags',
+  showCloseButton: true,
+  showCancelButton: true,
+  confirmButtonText:
+    '<i class="fa fa-thumbs-up"></i> Great!',
+  cancelButtonText:
+    '<i class="fa fa-thumbs-down"></i>'
+})
+}
+</script>
